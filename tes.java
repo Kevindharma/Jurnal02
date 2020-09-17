9ce0513b2f11f6b29f4ef41eafd3d2b4821aba29
Kevindharma <71244178+Kevindharma@users.noreply.github.com>
2020-09-17 13:36:34 +0700

    Delete tes.java

--- tes.java
+++
@@ -1,8 +0,0 @@
-public class tes {
-
-	public static void main(String[] args) {
-		System.out.println("Nama : Kevin!");
-		System.out.println("Nim  : 6706202124");
-		System.out.println("Kelas: 44-03");
-	}
-}
